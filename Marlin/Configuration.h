d5a81ee834dfc1f812c5b17b0f78479710f11950
kdobrev <kdobrev@mail.bg>
2023-12-01 21:06:47 +0100

    fixes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1246,7 +1246,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 500, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 500, 5000 }
 
 // #define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -1261,8 +1261,8 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          750    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  750    // E acceleration for retracts
+#define DEFAULT_ACCELERATION          1500    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   DEFAULT_ACCELERATION    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
